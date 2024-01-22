240fdc88e021d932c8cd8f65c51e640cc21cedf8
Alexandria Stephens <alexandria26999@gmail.com>
2024-01-21 20:21:54 -0700

    eightball text fixed, added transitions and timeout

--- src/Eightball.js
+++ src/Eightball.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { answers, selectRandom } from './answers.js';
 
-const Eightball = ({ answers }) => {
-    const [answer, setAnswer] = useState(selectRandom(answers));
+const Eightball = ({ answers = answers[0] }) => {
+    const [answer, setAnswer] = useState(answers[0]);
     const { msg, color } = answer;
-    console.log(msg, color)
-    return ( 
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setAnswer(answers[0]);
+        }, 4000);
+        return () => clearTimeout(timer);
+    });
+
+    return (
         <div className="eightball-container">
-            <div className="eightball-sphere" style={{backgroundColor : color}} 
-            onClick={() => setAnswer(selectRandom(answers))} >  
-                <h1 className="eightball-text" style={{color : "white"}}>{msg}</h1>
+            <div className="eightball-sphere" style={{ backgroundColor: color }}
+                onClick={() => setAnswer(selectRandom(answers))} >
+                <h1 className="eightball-text" style={{ color: "white" }}>{msg}</h1>
+                <div className="eightball-sphere-overlay">
+                </div>
             </div>
         </div>
-        )
+    )
 
 }
 
